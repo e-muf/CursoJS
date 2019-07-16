511d5e7c1b116f62c98fdd77c3595ae2192092a4
Emanuel Flores <mufymes@gmail.com>
2019-07-16 17:20:24 -0500

    Proyecto FETCH API Completo!

---
+++ PROYECTO Generador de Nombres FETCH API/js/app.js
@@ -0,0 +1,55 @@
+document.querySelector('#generar-nombre').addEventListener('submit', cargarNombres);
+
+// Llamado a AJAX e imprimir resultados
+function cargarNombres(event) {
+    event.preventDefault();
+
+    // Leer las variables
+    const origen = document.getElementById('origen');
+    const origenSeleccionado = origen.options[origen.selectedIndex].value;
+    
+    const genero = document.getElementById('genero');
+    const generoSeleccionado = genero.options[genero.selectedIndex].value;
+
+    const cantidad = document.getElementById('numero').value;
+
+    let url = ''
+    url += 'https://uinames.com/api/?';
+
+    // Si hay origen agregarlo a la url
+    if(origenSeleccionado !== '') {
+        url += `region=${origenSeleccionado}&`
+    }
+
+    // Si hay genero agregarlo a la url
+    if(generoSeleccionado !== '') {
+        url += `gender=${generoSeleccionado}&`
+    }
+
+    // Si hay una cantidad agregarlo a la url
+    if(cantidad !== '') {
+        url += `amount=${cantidad}`
+    }
+
+    // Crear FETCH
+    fetch(url)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            let html = `<h2 style="text-align: center">Nombres Generados</h2>`;
+            html+= `<ul class="lista">`;
+
+            data.forEach(nombre => {
+                html += `
+                    <li>${nombre.name}</li>
+                `;
+            });
+
+            html += `</ul>`;
+
+            document.querySelector('#resultado').innerHTML = html;
+        }).catch(function(error) {
+            console.log(error);
+        })
+}
